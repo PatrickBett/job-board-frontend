854f15b5debc4a0fddac5ee14f30e711445a50e0
Patrick <patrickbett018@gmail.com>
2024-10-05 19:29:22 +0300

    footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className="row">
+        <div className="col-md-4">
+            <h3>Terms And Conditions</h3>
+            <h5>termsand conditions</h5>
+        </div>
+
+        <div className="col-md-4">
+            <h3>Privacy Policy</h3>
+            <h5>All Rights Reserved</h5>
+        </div>
+
+        <div className="col-md-4">
+            <h3>Contact Us</h3>
+            <h5>Customer service - 0791474737</h5>
+        </div>
+    </div>
   )
 }
 
