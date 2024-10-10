e2ef131b4c7a5bb1c321b147c99144b9d275b4b6
Patrick <patrickbett018@gmail.com>
2024-10-10 17:03:14 +0300

    minor changes on structure

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,23 +1,30 @@
 import React from 'react'
 import "./footer.css";
+import { Link } from "react-router-dom";
+import CopyrightIcon from '@mui/icons-material/Copyright';
+import EmailIcon from '@mui/icons-material/Email';
 
-
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
 function Footer() {
   return (
     <div className="row text-white pt-5 pb-5 mt-5" id='footer'>
         <div className="col-md-4">
             <h5>Terms And Conditions</h5>
-            <p>termsand conditions</p>
+            <Link to='#' id='terms'>Terms and conditions of Use</Link>
         </div>
 
         <div className="col-md-4">
             <h5>Privacy Policy</h5>
-            <p>All Rights Reserved</p>
+            <CopyrightIcon />2024 - All Rights Reserved
         </div>
 
         <div className="col-md-4">
             <h5>Contact Us</h5>
-            <p>Customer service - 0791474737</p>
+            <LocalPhoneIcon />0791474737
+            
+            <br/>
+            <EmailIcon /> patrickbett018@gmail.com
+            
         </div>
     </div>
   )
--- src/Components/Landingpage.jsx
+++ src/Components/Landingpage.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import './landingpage.css'
 import { Link } from "react-router-dom";
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+
+
 function Landingpage() {
   return (
     <>
     <div className="container-fluid" id='landingpage'>  
-            <h1 className='p-3 fw-bolder'>Bring top talent to your business, your way</h1>
+            <h1 className='p-3 fw-bolder'>Bring top talent to your business, <strong>your way</strong></h1>
             <h5 className='p-3'>Build your dream team, fill skill gaps, and scale with our </h5>
             <h5 className='p-3'>full-service, customizable Enterprise platform. </h5>    
     </div>
@@ -16,16 +19,16 @@ function Landingpage() {
 </div>
 
         <div className="container mt-5 border p-5">
-        <h3 className='mb-2'>Unlimited Job Search Resources</h3>
+        <h3 className='mb-2'> <CheckCircleIcon />Unlimited Job Search Resources</h3>
             <p className='mb-5'>Full access to all features including unlimited jobs, articles, and webinars to help you with your remote job search.</p>
         
-            <h3 className='mb-2'>Higher Quality Listings
+            <h3 className='mb-2'><CheckCircleIcon />Higher Quality Listings
             </h3>
             <p className='mb-5'>Only legit jobs. No ads, scams, or junk to sift through. Our team spends 200+ hours/day verifying every job.
 
 </p>
         
-            <h3 className='mb-2'>Save Time</h3>
+            <h3 className='mb-2'><CheckCircleIcon />Save Time</h3>
             <p className='mb-5'>Go straight from job listings to applications. No more hopping from one job board to the next.</p>
         
             <button type='button' id='get-started'>
