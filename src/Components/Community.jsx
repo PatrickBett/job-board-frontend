4d17e68f848741b61efbc3941e7393639a56958a
Patrick <patrickbett018@gmail.com>
2024-10-10 19:09:25 +0300

    minor changes on structure

--- src/Components/Community.jsx
+++ src/Components/Community.jsx
@@ -1,19 +1,17 @@
 import React, { useEffect, useState } from "react";
 import "./community.css";
-
+import CheckCircleSharpIcon from '@mui/icons-material/CheckCircleSharp';
 import api from "../api";
 import Post from "./Post";
 
 function Community() {
   const [content, setContent] = useState("");
-  
-
   const [posts, setPosts] = useState([]);
+  const [showSuccessModal, setShowSuccessModal] = useState(false); // State for success modal
 
   useEffect(() => {
     getPosts();
   }, []);
-
 
   const getPosts = async () => {
     try {
@@ -26,34 +24,26 @@ function Community() {
     }
   };
 
-
-
-
   const createNewPost = async (e) => {
     e.preventDefault();
-    
+
     try {
-      const res = await api.post("/api/posts/", {content} );
-      console.log("res data",res.data);
-     
+      const res = await api.post("/api/posts/", { content });
+      console.log("res data", res.data);
+
       setPosts([...posts, res.data]); // Add the new post to the list
       setContent(""); // Clear the content after a successful post
-      // document.querySelector("#myModal .btn-close").click(); // Close modal
-      alert("Post added successfully");
-    } 
 
-    catch (error) {
+      setShowSuccessModal(true); // Show success modal
+    } catch (error) {
       alert("Error adding post");
-      // document.querySelector("#myModal .btn-close").click(); // Close modal
       console.error(error);
     }
-
   };
 
   return (
     <div className="container border ">
       <div className="row">
-        
         <div className="col-4">
           <button
             className="btn btn-success my-2"
@@ -105,6 +95,38 @@ function Community() {
           </div>
         </div>
       </div>
+
+      {/* Success Modal */}
+      <div className={`modal fade ${showSuccessModal ? "show" : ""}`} style={{ display: showSuccessModal ? "block" : "none" }} tabIndex="-1" role="dialog">
+        
+        <div className="modal-dialog" role="document">
+          <div className="modal-content" id='success-icon'>
+            <div className="modal-header">
+              <h5 className="modal-title" >Success</h5>
+              <button
+                type="button"
+                className="btn-close"
+                onClick={() => setShowSuccessModal(false)}
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+              <CheckCircleSharpIcon />
+              <p className="text-center">Post added successfully!</p>
+            </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-success"
+                onClick={() => setShowSuccessModal(false)}
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <div className="container mb-3">
         <Post posts={posts} />
       </div>
